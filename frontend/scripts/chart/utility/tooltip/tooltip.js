5d0c1eafd7b515b129948c3fd40483d458874945
Bepely <ngogin7@gmail.com>
2023-02-22 12:08:26 +0300

    Minimum test requirements DONE
    Выполнил все минимальные требования из тестового, но это все еще выглядит как помойка.
    Дальше только наветсти порядок в коде, архитектуре и сделать тестовое красивым

--- frontend/scripts/chart/utility/tooltip/tooltip.js
+++ frontend/scripts/chart/utility/tooltip/tooltip.js
@@ -5,6 +5,14 @@ export const createTooltip = (svg, minY, maxY, h) => {
   //make a tooltip config
   let tooltipX, tooltipY, tooltipW, tooltipH;
 
+  let backTooltip = getRect(0, 0, 1024 * 0.1, 512 * 0.1);
+  backTooltip.setAttribute("fill", "aqua");
+  backTooltip.style.pointerEvents = "none";
+  backTooltip.style.position = "relative";
+  backTooltip.style.zIndex = 0;
+  backTooltip.style.display = "none";
+  svg.appendChild(backTooltip);
+
   //create an instance of a tooltip
   let tooltip = getText1();
   tooltip.setAttribute("x", 100);
@@ -13,6 +21,9 @@ export const createTooltip = (svg, minY, maxY, h) => {
   tooltip.setAttribute("fill", "black");
   tooltip.textContent = "Shalom";
   tooltip.style.pointerEvents = "none";
+  tooltip.style.position = "relative";
+  tooltip.style.zIndex = 1;
+  tooltip.style.display = "none";
   svg.appendChild(tooltip);
 
   //get a range of values
@@ -35,16 +46,22 @@ export const createTooltip = (svg, minY, maxY, h) => {
       y > 10 &&
       y < Math.floor(triggerBox.height.animVal.value) - 10
     ) {
+      //show a text of a tooltip
       tooltip.style.display = "block";
-      tooltip.setAttribute("y", y);
-      //maxY - (crosshairPositionY / yAxisHeight) * yRange;
-      tooltip.textContent = (maxY - (y / (h * 0.9)) * yRange).toFixed(1);
+      tooltip.setAttribute("y", y + h * 0.1);
+      tooltip.setAttribute("x", x + 1024 * 0.1);
+
+      tooltip.textContent = `Y: ${(maxY - (y / (h * 0.9)) * yRange).toFixed(
+        1
+      )}`;
+
+      //show a background of a tooltip
+      backTooltip.style.display = "block";
+      backTooltip.setAttribute("y", y + h * 0.3 * 0.1);
+      backTooltip.setAttribute("x", x + 1024 * 0.9 * 0.1);
     } else {
       tooltip.style.display = "none";
+      backTooltip.style.display = "none";
     }
   });
 };
-
-export const setTooltip = (tooltip, minY, maxY) => {};
-
-export const getTooltip = () => {};
